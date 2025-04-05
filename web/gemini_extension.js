6945a20220f410ff8fe4d813bb1f351b00426105
ImpactFrames <jvoxel@gmail.com>
2025-04-05 12:05:15 +0800

    Add files via upload

--- web/gemini_extension.js
+++ web/gemini_extension.js
@@ -1,6 +1,7 @@
 // gemini_extension.js - Main entry point for Gemini Node extension
 import { app } from "/scripts/app.js";
 import "./js/gemini_node.js";
+import "./js/gemini_sequence.js";
 
 app.registerExtension({
     name: "ComfyUI.IF_Gemini.Main",
